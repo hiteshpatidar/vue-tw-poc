3752380a47b3eec42569b517d807a28badaf3d08
kincare <hitesh.patidar@cygnetinfotech.com>
2021-08-12 11:50:11 +0530

    Revert "codeflask editor"
    This reverts commit 568da1a75a36b171ac234d47f61b1b370a0c9d61.

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,22 @@
 <template>
   <div id="app">
-    <javascript-editor></javascript-editor>
-    <!-- <monaco-editor-demo></monaco-editor-demo> -->
-    <!-- <new-editor></new-editor> -->
+    <img
+      alt="Vue logo"
+      height="52px"
+      width="260px"
+      src="./assets/TestingWhiz-logo.png"
+    />
+    <HelloWorld msg="Welcome to Your TestingWhiz App" />
   </div>
 </template>
 
 <script>
-// import NewEditor from "./components/NewEditor.vue";
-// import MonacoEditorDemo from "./components/MonacoEditorDemo.vue";
-import JavascriptEditor from "./components/JavascriptEditor.vue";
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
   name: "App",
   components: {
-    // NewEditor
-    // MonacoEditorDemo
-    JavascriptEditor
+    HelloWorld
   }
 };
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,11 @@
-// const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
 module.exports = {
-  transpileDependencies: []
-  // chainWebpack: config => {
-  //   config.module
-  //     .rule("eslint")
-  //     .use("eslint-loader")
-  //     .options({
-  //       fix: true
-  //     });
-  // }
-  // chainWebpack: config => {
-  //   config.plugin("monaco-editor").use(MonacoWebpackPlugin, [
-  //     {
-  //       // Languages are loaded on demand at runtime
-  //       languages: ["json", "javascript", "html", "xml"]
-  //     }
-  //   ]);
-  // }
+  transpileDependencies: [],
+  chainWebpack: config => {
+    config.module
+      .rule("eslint")
+      .use("eslint-loader")
+      .options({
+        fix: true
+      });
+  }
 };
